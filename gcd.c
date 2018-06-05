f411bfa184184185543b975a9b65059a9d0031b4
shdxiang <shdxiang@qq.com>
2018-06-05 14:37:02 +0800

    add gcd.c

---
+++ gcd.c
@@ -0,0 +1,28 @@
+// https://en.wikipedia.org/wiki/Euclidean_algorithm
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int i, int j) {
+  if (j == 0) {
+    return i;
+  } else {
+    return gcd(j, i % j);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  if (argc < 2) {
+    printf("usage: %s <integer> <integer>\n", argv[0]);
+    return 0;
+  }
+
+  int i = atol(argv[1]);
+  int j = atol(argv[2]);
+
+  int divisor = gcd(i, j);
+
+  printf("%d\n", divisor);
+
+  return 0;
+}
